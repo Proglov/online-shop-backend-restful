dfb8ad96d3d2ce271c385a44cc294493113efcf0
Proglov <dev.mohammad.parvizi@gmail.com>
2024-08-12 16:53:15 +0330

    updated the cors option

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -14,7 +14,8 @@
 //     optionsSuccessStatus: 200
 // }
 
-var allowlist = ["http://localhost:3000", "https://shopping-front-gamma.vercel.app"]
+// var allowlist = ["http://localhost:3000", "https://onlineshopiranian.liara.run"]
+var allowlist = ["https://onlineshopiranian.liara.run"]
 
 var corsOptions = function (req, callback) {
     var corsOptionsDelegate;
