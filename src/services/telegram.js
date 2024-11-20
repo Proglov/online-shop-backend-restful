146fddbdfce4dff28b1bcb652cae312b05949b78
Proglov <dev.mohammad.parvizi@gmail.com>
2024-11-20 22:43:09 +0330

    added try catch to sendNotification

--- src/services/telegram.js
+++ src/services/telegram.js
@@ -4,14 +4,18 @@ const api = process.env.TelegramBot_API
 
 
 const sendNotification = async (obj) => {
-    const response = await fetch(api + '/notif/sendNotification', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ ...obj, auth: "IMADMIN" })
-    })
-    return response
+    try {
+        const response = await fetch(api + '/notif/sendNotification', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ ...obj, auth: "IMADMIN" })
+        })
+        return response
+    } catch (error) {
+        return null
+    }
 }
 
 module.exports = {
