9766c8d97dd5da07322b12dca4062dc6151164b4
Proglov <dev.mohammad.parvizi@gmail.com>
2024-12-15 16:18:06 +0330

    set secure and path and samesite for the cookie

--- src/routes/city/post.js
+++ src/routes/city/post.js
@@ -21,13 +21,20 @@ router.post('/set-cityIds', (req, res) => {
     const { ids } = req.body;
 
     if (Array.isArray(ids)) {
-        // Set a cookie with the key "IDs" and value as the JSON string of the array
-        res.cookie('cityIds', JSON.stringify(ids), { httpOnly: false, maxAge: 86400000 }); // Max age is set to 1 day
+        // Set a cookie with the key "cityIds" and value as the JSON string of the array
+        res.cookie('cityIds', JSON.stringify(ids), {
+            httpOnly: false,
+            maxAge: 86400000,
+            path: '/',
+            secure: true,
+            sameSite: 'None'
+        });
         return res.status(200).send('Cookie has been set!');
     } else {
         return res.status(400).send('Invalid input. Please send an array.');
     }
 });
+
 
 
 module.exports = router;
